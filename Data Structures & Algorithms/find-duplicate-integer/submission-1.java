8314789b113df07da50a7e90d685952aa76fe64b
MD ZIYAD HUSSAIN <ziyad.hussain23b@iiitg.ac.in>
2026-07-21 19:23:46 +0530

    Add: find-duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-1.java
@@ -0,0 +1,25 @@
+class Solution {
+    // make the input array itself hashmap
+    // public int findDuplicate(int[] nums) {
+    //     for(int i = 0; i < nums.length; i++){
+    //         if(nums[Math.abs(nums[i]) - 1] < 0) return Math.abs(nums[i]);
+    //         nums[Math.abs(nums[i]) - 1] *= -1;
+    //     }
+    //     return -1;
+    // }
+
+    public int findDuplicate(int[] nums) {
+        int slow = 0, fast = 0;
+        while(true){
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if(slow == fast) break;
+        }
+        int slow2 = 0;
+        while(true){
+            slow = nums[slow];
+            slow2 = nums[slow2];
+            if(slow == slow2) return slow;
+        }
+    }
+}
