1d5468b5ef83a678cb8ffbc06d6c59c65401a76f
MD ZIYAD HUSSAIN <ziyad.hussain23b@iiitg.ac.in>
2026-07-23 21:20:53 +0530

    Add: kth-largest-element-in-an-array - submission-0

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-0.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        Queue<Integer> pq = new PriorityQueue<>();
+        for(int i : nums){
+            pq.offer(i);
+            if(pq.size() > k) pq.poll();
+        }
+        return pq.peek();
+    }
+}
