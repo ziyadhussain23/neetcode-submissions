ccdaed6db5e0ec2114075562fa499ff4ba395f8a
MD ZIYAD HUSSAIN <ziyad.hussain23b@iiitg.ac.in>
2026-07-23 20:36:57 +0530

    Add: kth-largest-integer-in-a-stream - submission-2

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-2.java
@@ -0,0 +1,48 @@
+class KthLargest {
+    // int k;
+    // Queue<Integer> pq;
+
+    // public KthLargest(int k, int[] nums) {
+    //     this.k = k;
+    //     pq = new PriorityQueue<>((a, b) -> a - b);
+    //     for(int i = 0; i < nums.length; i++){
+    //         if(pq.size() == k){
+    //             if(pq.peek() < nums[i]){
+    //                 pq.poll();
+    //                 pq.offer(nums[i]);
+    //             }
+    //         }else
+    //             pq.offer(nums[i]);
+    //     }
+    // }
+
+    // public int add(int val) {
+    //     if(pq.size() == k){
+    //         if(pq.peek() < val){
+    //             pq.poll();
+    //             pq.offer(val);
+    //         }
+    //     }else
+    //         pq.offer(val);
+    //     return pq.peek();
+    // }
+
+
+    int k;
+    Queue<Integer> pq;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        pq = new PriorityQueue<>();
+        for(int i : nums){
+            pq.offer(i);
+            if(pq.size() > k) pq.poll();
+        }
+    }
+
+    public int add(int val) {
+        pq.offer(val);
+        if(pq.size() > k) pq.poll();
+        return pq.peek();
+    }
+}
