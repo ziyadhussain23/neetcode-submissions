c1b7e423865dd7bd7a491334029ca020d78ff75d
MD ZIYAD HUSSAIN <ziyad.hussain23b@iiitg.ac.in>
2026-07-27 16:57:57 +0530

    Add: palindrome-partitioning - submission-0

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-0.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> res = new ArrayList<>();
+        helper(s, 0, 0, new ArrayList<>(), res);
+        return res;
+    }
+
+    private boolean valid(String s){
+        if(s.isEmpty()) return false;
+        for(int i = 0, j = s.length() - 1; i < j; i++, j--){
+            if(s.charAt(i) != s.charAt(j)) return false;
+        }
+        return true;
+    }
+
+    private void helper(String s, int st, int idx, List<String> cur, List<List<String>> res){
+        String sub = st < s.length() && idx <= s.length() ? s.substring(st, idx) : "";
+        if(idx == s.length()){
+            if(valid(sub)){
+                cur.add(sub);
+                res.add(new ArrayList<>(cur));
+                cur.removeLast();
+            }
+            return;
+        }
+
+        if(valid(sub)){
+            cur.add(sub);
+            helper(s, idx, idx, cur, res);
+            cur.removeLast();
+        }
+        helper(s, st, idx + 1, cur, res);
+    }
+}
