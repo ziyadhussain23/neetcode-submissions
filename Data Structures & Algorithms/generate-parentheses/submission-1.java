dbbee8caeeda4ae790492e6356765b99f010d051
MD ZIYAD HUSSAIN <ziyad.hussain23b@iiitg.ac.in>
2026-07-27 12:03:03 +0530

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        helper(n, 0, new StringBuilder(), res);
+        return res;
+    }
+
+    private void helper(int n, int cnt, StringBuilder str, List<String> res){
+        if(cnt > n) return;
+        if(str.length() == 2 * n){
+            if(cnt == 0) res.add(str.toString());
+            return;
+        }
+
+        str.append('(');
+        helper(n, cnt + 1, str, res);
+        str.setLength(str.length() - 1);
+        if(cnt > 0){
+            str.append(')');
+            helper(n, cnt - 1, str, res);
+            str.setLength(str.length() - 1);
+        }
+    }
+}
