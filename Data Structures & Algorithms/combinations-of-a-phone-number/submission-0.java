763091941d8a2b4ea3a17aef7cd1ee07e97178ec
MD ZIYAD HUSSAIN <ziyad.hussain23b@iiitg.ac.in>
2026-07-27 17:12:53 +0530

    Add: combinations-of-a-phone-number - submission-0

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-0.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        if(digits == null || digits.isEmpty()) return new ArrayList<>();
+       String[] num = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        List<String> res = new ArrayList<>();
+        helper(digits, 0, num, new StringBuilder(), res);
+        return res;
+    }
+
+    private void helper(String s, int idx, String[] num, StringBuilder cur, List<String> res){
+        if(idx == s.length() || cur.length() == s.length()){
+            res.add(cur.toString());
+            return;
+        }
+
+        for(char c : num[s.charAt(idx) - '0'].toCharArray()){
+            cur.append(c);
+            helper(s, idx + 1, num, cur, res);
+            cur.setLength(cur.length() - 1);
+        }
+    }
+}
